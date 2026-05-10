93b8cab670a27b5a30194ec87b42b3d7efed6096
Donald J. Fortier II <Technical_13@yahoo.com>
2026-05-10 14:27:44 -0400

    Replace console logs with logger for edit success/failure

--- src/api/edit.js
+++ src/api/edit.js
@@ -9,11 +9,11 @@ export class WikiEdit {
     try {
       if ( !params.token ) params.token = await this.session.tokens.get( 'csrf' );
       const res = await this.session._post( { action: 'edit', ...params } );
-      if ( res.edit && res.edit.result === 'Success' ) console.log( '[Wiki] Edit successful on: ' + res.edit.title );
+      if ( res.edit && res.edit.result === 'Success' ) this.session.logger.info( '[Wiki] Edit successful on: ' + res.edit.title );
       return res.edit;
     }
     catch ( e ) {
-      console.error( '[Wiki] Edit failure: ' + e.message );
+      this.session.logger.error( '[Wiki] Edit failure: ' + e.message );
       return null;
     }
   }
