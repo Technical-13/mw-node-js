3150296ada6a09572777e7e8dfb596a79d4924cc
Donald J. Fortier II <Technical_13@yahoo.com>
2026-05-10 13:34:27 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
-// src/index.js
+import { WikiSession } from './api/auth.js';
+import { WikiQuery } from './api/query.js';
+import { WikiEdit } from './api/edit.js';
+import { WikiAccount } from './api/account.js';
+import { WikiMaintenance } from './api/maintenance.js';
+import { WikiMedia } from './api/media.js';
+import { WikiParser } from './api/parser.js';
+import { WikiWatchlist } from './api/watchlist.js';
 
-// Export the Auth module
-export { WikiSession } from './api/auth.js';
-
+export {
+  WikiSession,
+  WikiQuery,
+  WikiEdit,
+  WikiAccount,
+  WikiMaintenance,
+  WikiMedia,
+  WikiParser,
+  WikiWatchlist
+};
