99fe37a333c015c6ed7ded9075d3e6f74646a7be
Donald J. Fortier II <Technical_13@yahoo.com>
2026-05-10 13:29:17 -0400

    Add WikiWatchlist class for managing watchlist
    Implemented WikiWatchlist class to manage watchlist actions.

---
+++ src/api/watchlist.js
@@ -0,0 +1,24 @@
+/**
+ * WikiWatchlist Class
+ * Manages the bot\'s watchlist (adding or removing pages).
+ */
+export class WikiWatchlist {
+  constructor( session ) { this.session = session; }
+
+  async watch( params ) {
+    try {
+      if ( !params.token ) params.token = await this.session.tokens.get( 'watch' );
+      const res = await this.session._post( { action: 'watch', ...params } );
+      if ( res.watch ) console.log( '[Wiki] Watchlist updated for: ' + res.watch.title );
+      return res.watch;
+    }
+    catch ( e ) {
+      console.error( '[Wiki] Watch failure: ' + e.message );
+      return null;
+    }
+  }
+
+  async unwatch( params ) {
+    return await this.watch( { ...params, unwatch: true } );
+  }
+}
