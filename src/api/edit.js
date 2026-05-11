ecf0e198c141b38b69888057f591d7748dad4b76
Donald J. Fortier II <Technical_13@yahoo.com>
2026-05-11 07:17:51 -0400

    Add spamblacklist and titleblacklist methods

--- src/api/edit.js
+++ src/api/edit.js
@@ -60,6 +60,36 @@ export class WikiEdit {
   }
 
   /**
+   * Validates one or more URLs against the spam block list.
+   * @param {Object} params - Parameters for the spamblacklist action.
+   * @returns {Promise} Result of the validation.
+   */
+  async spamblacklist( params ) {
+    try {
+      const res = await this.session._post( { action: 'spamblacklist', ...params } );
+      return res.spamblacklist;
+    } catch ( e ) {
+      this.session.logger.error( '[Wiki] Spam blacklist check failure: ' + e.message );
+      return null;
+    }
+  }
+
+  /**
+   * Validates a title, filename, or username against the title blacklist.
+   * @param {Object} params - Parameters for the titleblacklist action.
+   * @returns {Promise} Result of the validation.
+   */
+  async titleblacklist( params ) {
+    try {
+      const res = await this.session._post( { action: 'titleblacklist', ...params } );
+      return res.titleblacklist;
+    } catch ( e ) {
+      this.session.logger.error( '[Wiki] Title blacklist check failure: ' + e.message );
+      return null;
+    }
+  }
+
+  /**
    * Uploads a file to the wiki.
    * @param {Object} params - Parameters for the upload action.
    * @returns {Promise} Result of the upload action.
